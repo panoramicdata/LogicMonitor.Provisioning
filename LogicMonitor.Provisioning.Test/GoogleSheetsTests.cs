3649da15771c141248e5b149b65bf7b21074667e
David Bond <david.bond@panoramicdata.com>
2024-12-08 16:43:11 +0000

    Moved to file-scoped namespaces throughout.

--- LogicMonitor.Provisioning.Test/GoogleSheetsTests.cs
+++ LogicMonitor.Provisioning.Test/GoogleSheetsTests.cs
@@ -1,34 +1,33 @@
 using FluentAssertions;
 using LogicMonitor.Provisioning.GoogleSheets;
 
-namespace LogicMonitor.Provisioning.Test
+namespace LogicMonitor.Provisioning.Test;
+
+public class GoogleSheetsTests
 {
-	public class GoogleSheetsTests
+	[Fact]
+	public async Task DownloadSucceeds()
 	{
-		[Fact]
-		public async Task DownloadSucceeds()
+		var googleSheetLoader = new GoogleFileDownloader("1uqwtDMMh1zqTkmJHXf1Y_fS0ZUWr6OHr|Install Order");
+
+		// Get a temporary fileInfo
+		var tempFileInfo = new FileInfo(Path.GetTempFileName() + ".xlsx");
+
+		try
 		{
-			var googleSheetLoader = new GoogleFileDownloader("1uqwtDMMh1zqTkmJHXf1Y_fS0ZUWr6OHr|Install Order");
+			var fileAndSheetInfo = await googleSheetLoader
+				.DownloadAsync(tempFileInfo, CancellationToken.None);
 
-			// Get a temporary fileInfo
-			var tempFileInfo = new FileInfo(Path.GetTempFileName() + ".xlsx");
+			// Check the file exists
+			tempFileInfo.Exists.Should().BeTrue();
 
-			try
-			{
-				var fileAndSheetInfo = await googleSheetLoader
-					.DownloadAsync(tempFileInfo, CancellationToken.None);
-
-				// Check the file exists
-				tempFileInfo.Exists.Should().BeTrue();
-
-				// Check the sheet name
-				fileAndSheetInfo.SheetName.Should().Be("Install Order");
-			}
-			finally
-			{
-				// Clean up
-				tempFileInfo.Delete();
-			}
+			// Check the sheet name
+			fileAndSheetInfo.SheetName.Should().Be("Install Order");
+		}
+		finally
+		{
+			// Clean up
+			tempFileInfo.Delete();
 		}
 	}
 }
--- LogicMonitor.Provisioning/GoogleSheets/GoogleFileDownloader.cs
+++ LogicMonitor.Provisioning/GoogleSheets/GoogleFileDownloader.cs
@@ -3,91 +3,90 @@ using Google.Apis.Drive.v3;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
 
-namespace LogicMonitor.Provisioning.GoogleSheets
+namespace LogicMonitor.Provisioning.GoogleSheets;
+
+public class GoogleFileDownloader : IDisposable
 {
-	public class GoogleFileDownloader : IDisposable
+	private DriveService? service;
+	private bool disposedValue;
+	private readonly string _fileId;
+	private readonly string _sheetName;
+
+	public GoogleFileDownloader(string? evaluatedConfig)
 	{
-		private DriveService? service;
-		private bool disposedValue;
-		private readonly string _fileId;
-		private readonly string _sheetName;
-
-		public GoogleFileDownloader(string? evaluatedConfig)
+		if (string.IsNullOrWhiteSpace(evaluatedConfig))
 		{
-			if (string.IsNullOrWhiteSpace(evaluatedConfig))
+			throw new ConfigurationException("Repetition config for file should be in the form 'fileId|Sheetname'.");
+		}
+
+		var configDetails = evaluatedConfig.Split('|') ?? throw new InvalidOperationException("Config should have been validated.  Found missing Repetition.Config");
+		if (configDetails.Length != 2)
+		{
+			throw new ConfigurationException("Repetition config for file should be in the form 'fileId|Sheetname'.");
+		}
+
+		_fileId = configDetails[0];
+		_sheetName = configDetails[1];
+	}
+
+	private async Task<DriveService> GetServiceAsync(CancellationToken cancellationToken)
+	{
+		if (service != null)
+		{
+			return service;
+		}
+
+		UserCredential credential;
+
+		await using var stream = new FileStream("googleSheets.json", FileMode.Open, FileAccess.Read);
+		var secrets = await GoogleClientSecrets.FromStreamAsync(stream, cancellationToken);
+
+		credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+			secrets.Secrets,
+			[DriveService.Scope.Drive],
+			"user",
+			cancellationToken,
+			new FileDataStore("token.json", true)
+			);
+
+		// Create Drive API service.
+		return service = new DriveService(new BaseClientService.Initializer()
+		{
+			HttpClientInitializer = credential,
+			ApplicationName = "LogicMonitor.Provisioning",
+		});
+	}
+
+	internal async Task<FileAndSheetInfo> DownloadAsync(FileInfo fileInfo, CancellationToken cancellationToken)
+	{
+		var service = await GetServiceAsync(cancellationToken);
+		await using var stream = new FileStream(fileInfo.FullName, FileMode.Create, FileAccess.Write);
+		await service
+			.Files
+			.Get(_fileId)
+			.DownloadAsync(stream, cancellationToken);
+
+		return new FileAndSheetInfo($"{fileInfo.FullName}|{_sheetName}");
+	}
+
+	protected virtual void Dispose(bool disposing)
+	{
+		if (!disposedValue)
+		{
+			if (disposing)
 			{
-				throw new ConfigurationException("Repetition config for file should be in the form 'fileId|Sheetname'.");
+				// TODO: dispose managed state (managed objects)
+				service?.Dispose();
 			}
 
-			var configDetails = evaluatedConfig.Split('|') ?? throw new InvalidOperationException("Config should have been validated.  Found missing Repetition.Config");
-			if (configDetails.Length != 2)
-			{
-				throw new ConfigurationException("Repetition config for file should be in the form 'fileId|Sheetname'.");
-			}
-
-			_fileId = configDetails[0];
-			_sheetName = configDetails[1];
+			disposedValue = true;
 		}
+	}
 
-		private async Task<DriveService> GetServiceAsync(CancellationToken cancellationToken)
-		{
-			if (service != null)
-			{
-				return service;
-			}
-
-			UserCredential credential;
-
-			await using var stream = new FileStream("googleSheets.json", FileMode.Open, FileAccess.Read);
-			var secrets = await GoogleClientSecrets.FromStreamAsync(stream, cancellationToken);
-
-			credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
-				secrets.Secrets,
-				[DriveService.Scope.Drive],
-				"user",
-				cancellationToken,
-				new FileDataStore("token.json", true)
-				);
-
-			// Create Drive API service.
-			return service = new DriveService(new BaseClientService.Initializer()
-			{
-				HttpClientInitializer = credential,
-				ApplicationName = "LogicMonitor.Provisioning",
-			});
-		}
-
-		internal async Task<FileAndSheetInfo> DownloadAsync(FileInfo fileInfo, CancellationToken cancellationToken)
-		{
-			var service = await GetServiceAsync(cancellationToken);
-			await using var stream = new FileStream(fileInfo.FullName, FileMode.Create, FileAccess.Write);
-			await service
-				.Files
-				.Get(_fileId)
-				.DownloadAsync(stream, cancellationToken);
-
-			return new FileAndSheetInfo($"{fileInfo.FullName}|{_sheetName}");
-		}
-
-		protected virtual void Dispose(bool disposing)
-		{
-			if (!disposedValue)
-			{
-				if (disposing)
-				{
-					// TODO: dispose managed state (managed objects)
-					service?.Dispose();
-				}
-
-				disposedValue = true;
-			}
-		}
-
-		public void Dispose()
-		{
-			// Do not change this code. Put clean up code in 'Dispose(bool disposing)' method
-			Dispose(disposing: true);
-			GC.SuppressFinalize(this);
-		}
+	public void Dispose()
+	{
+		// Do not change this code. Put clean up code in 'Dispose(bool disposing)' method
+		Dispose(disposing: true);
+		GC.SuppressFinalize(this);
 	}
 }
